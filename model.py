a6c1f94bcd0d97730b3a1095dad24b8fdd9d1e41
Oleg_bul <kokseg63@gmail.com>
2022-12-29 21:24:11 +0400

    upper and rub added

--- model.py
+++ model.py
@@ -2,5 +2,8 @@ import requests
 
 
 def get_valute(code):
-    data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-    return data['Valute'][code]['Value']
+    if code.upper() == 'RUB':
+        return 1
+    else:
+        data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
+        return data['Valute'][code.upper()]['Value']
